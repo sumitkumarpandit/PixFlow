7e137a5a3b6c76773b41853d5c58eba25c54e5fe
sumitkumarpandit <sumitkumarpandit434@gmail.com>
2024-04-17 06:50:51 +0530

    update image cache and load libraries file

--- ImageFluxLibrary/src/main/java/com/sumitkumarpandit/imagefluxlibrary/ImageCache.kt
+++ ImageFluxLibrary/src/main/java/com/sumitkumarpandit/imagefluxlibrary/ImageCache.kt
@@ -10,25 +10,21 @@ import java.io.IOException
 object ImageCache {
     private const val CACHE_DIRECTORY = "image_cache"
 
-    fun saveBitmap(context: Context, url: String, bitmap: Bitmap) {
-        val cacheDir = File(context.cacheDir, CACHE_DIRECTORY)
-        if (!cacheDir.exists()) {
-            cacheDir.mkdirs()
+    // saving the image in internal storage
+    fun storeBitmap(bitmap: Bitmap, context: Context, fileName: String) {
+        val directory = File(context.filesDir, "CACHE_DIRECTORY")
+        if (!directory.exists()) {
+            directory.mkdirs() // Create the directory if it doesn't exist
         }
-        val file = File(cacheDir, url.hashCode().toString())
-        try {
-            val outputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-            outputStream.flush()
-            outputStream.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+        val fileOutputStream = FileOutputStream(File(directory, fileName))
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
+        fileOutputStream.flush()
+        fileOutputStream.close()
     }
 
-    fun getBitmap(context: Context, url: String): Bitmap? {
-        val cacheDir = File(context.cacheDir, CACHE_DIRECTORY)
-        val file = File(cacheDir, url.hashCode().toString())
+    // loading the cached image from internal storage
+    fun loadBitmap(context: Context, fileName: String): Bitmap? {
+        val file = File(context.filesDir, "CACHE_DIRECTORY/$fileName")
         return if (file.exists()) {
             BitmapFactory.decodeFile(file.absolutePath)
         } else {
--- ImageFluxLibrary/src/main/java/com/sumitkumarpandit/imagefluxlibrary/ImageLoader.kt
+++ ImageFluxLibrary/src/main/java/com/sumitkumarpandit/imagefluxlibrary/ImageLoader.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import java.io.IOException
 import java.lang.Exception
 import java.net.HttpURLConnection
@@ -12,42 +13,8 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 object ImageLoader {
-    private val cache = HashMap<String, Bitmap>()
-    private val executor: ExecutorService = Executors.newFixedThreadPool(5)
-    private val handler = Handler(Looper.getMainLooper())
-
-    fun loadImage(url: String, callback: (Bitmap?) -> Unit) {
-        if (cache.containsKey(url)) {
-            callback(cache[url])
-        } else {
-            executor.execute {
-                try {
-                    val bitmap = downloadBitmap(url)
-                    cache[url] = bitmap!!
-                    handler.post { callback(bitmap) }
-                } catch (e: Exception) {
-                    handler.post { callback(null) }
-                }
-            }
-        }
-    }
-
-    private fun downloadBitmap(url: String): Bitmap? {
-        var bitmap: Bitmap? = null
-        var connection: HttpURLConnection? = null
-        try {
-            val imageUrl = URL(url)
-            connection = imageUrl.openConnection() as HttpURLConnection
-            connection.connectTimeout = 10000
-            connection.readTimeout = 10000
-            connection.connect()
-            val inputStream = connection.inputStream
-            bitmap = BitmapFactory.decodeStream(inputStream)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            connection?.disconnect()
-        }
-        return bitmap
+    fun getBitmapFromURL(imageUrl:String): Bitmap? {
+        val url = URL(imageUrl)
+        return BitmapFactory.decodeStream(url.openConnection().getInputStream())
     }
 }
