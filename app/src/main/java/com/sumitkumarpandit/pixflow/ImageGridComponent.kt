88c2dc66986967651cb3710730909b74743adbd9
sumitkumarpandit <sumitkumarpandit434@gmail.com>
2024-04-21 12:49:47 +0530

    image grid component

---
+++ app/src/main/java/com/sumitkumarpandit/pixflow/ImageGridComponent.kt
@@ -0,0 +1,76 @@
+package com.sumitkumarpandit.pixflow
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
+import com.sumitkumarpandit.imagefluxlibrary.ImageCache
+import com.sumitkumarpandit.imagefluxlibrary.ImageLoader
+import com.sumitkumarpandit.pixflow.data.UPictures
+import com.sumitkumarpandit.pixflow.models.PixFlowViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun ImageGrid(viewModel: PixFlowViewModel, imageList: List<UPictures>, context: Context) {
+    val scrollState = rememberLazyGridState()
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        contentPadding = PaddingValues(4.dp),
+        modifier = Modifier.fillMaxSize(),
+        state = scrollState
+    ) {
+        items(imageList.size) { idx ->
+            val coroutineScope = rememberCoroutineScope()
+            var bitmapState by remember { mutableStateOf<Bitmap?>(null) }
+
+            LaunchedEffect(coroutineScope) {
+                this.launch {
+                    val bitmap =
+                        loadBitmap(imageList[idx].urls.regular, context, imageList[idx].id)
+                    bitmapState = bitmap
+                }
+            }
+            ImageItem(imageBitmap = bitmapState?.asImageBitmap())
+            if (idx == imageList.size - 1) {
+                LaunchedEffect(scrollState) {
+                    if (idx == scrollState.layoutInfo.totalItemsCount - 1) {
+                        viewModel.getPictureUrls(BuildConfig.API_KEY)
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+suspend fun loadBitmap(url: String, context: Context, id: String): Bitmap? {
+    return withContext(Dispatchers.IO) {
+        val cachedBitmap = ImageCache.loadBitmap(context, id)
+        if (cachedBitmap != null) {
+            cachedBitmap
+        } else {
+            val bitmapFromUrl = ImageLoader.getBitmapFromURL(url)
+            bitmapFromUrl?.let {
+                ImageCache.storeBitmap(it, context, id)
+                it
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/sumitkumarpandit/pixflow/ImageGridItem.kt
@@ -0,0 +1,52 @@
+package com.sumitkumarpandit.pixflow
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ImageItem(imageBitmap: ImageBitmap?) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(120.dp, 180.dp)
+            .padding(4.dp)
+            .clip(RoundedCornerShape(4.dp))
+    ) {
+        imageBitmap?.let {
+            Image(
+                bitmap = it,
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds
+            )
+        } ?: run {
+            ShimmerItem()
+        }
+
+    }
+}
+
+
+@Composable
+fun ShimmerItem() {
+    Surface(color = Color.LightGray) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .shimmerEffect()
+        )
+    }
+}
